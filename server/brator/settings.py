3ecb1c3d2f9da7ffdb6211bdbb72a3aa2dcdd654
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2021-03-12 18:34:36 +0100

    add whitenoise for static file serving

--- server/brator/settings.py
+++ server/brator/settings.py
@@ -35,13 +35,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'brator.urls'
@@ -71,7 +71,7 @@ DATABASES = {
         "USER": "brator_test",
         "HOST": "localhost",
         "PORT": "5432",
-        "PASSWORD": os.environ["BRATOR_DB_PASSWORD"],
+        "PASSWORD": os.environ.get("BRATOR_DB_PASSWORD"),
     }
 }
 
@@ -125,3 +125,5 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 50,
     "COMPACT_JSON": False,
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
