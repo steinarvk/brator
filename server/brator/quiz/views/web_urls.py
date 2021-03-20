590197ad53c4529fd73bc10d5b33980c3d36676d
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2021-03-20 18:58:00 +0100

    redo URL scheme

--- server/brator/quiz/views/web_urls.py
+++ server/brator/quiz/views/web_urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from . import web
 
 urlpatterns = [
-    path("quiz/", web.quiz, name="web-quiz"),
-    path("quiz/skip/", web.skip_question, name="web-skip"),
-    path("eval/", web.eval_results, name="web-eval"),
-    path("feedback/<uid>/question/", web.question_feedback, name="web-report-question"),
-    path("feedback/<uid>/answer/", web.answer_feedback, name="web-report-answer"),
+    path("practice/", web.quiz, name="web-quiz"),
+    path("practice/skip/", web.skip_question, name="web-skip"),
+    path("analyze/", web.eval_results, name="web-eval"),
+    path("practice/feedback/<uid>/question/", web.question_feedback, name="web-report-question"),
+    path("practice/feedback/<uid>/answer/", web.answer_feedback, name="web-report-answer"),
     path("account/", web.account_settings, name="web-account"),
     path("account/delete/", web.delete_account, name="web-deleteaccount"),
     path("", web.index, name="web-index"),
