32635858596b844898d6c90e9578bc3846b59e7a
Steinar V. Kaldager <steinarvkaldager@gmail.com>
2021-03-20 15:34:16 +0100

    improve test database script

--- testenv/run-database.sh
+++ testenv/run-database.sh
@@ -3,4 +3,5 @@
 POSTGRES_IMAGE=postgres:12
 BRATOR_TEST_DIR=/tmp/brator_test_data
 
-docker run -e POSTGRES_USER=${BRATOR_DB_USER} -e POSTGRES_PASSWORD=${BRATOR_DB_PASSWORD} -p 5432:5432 -v ${BRATOR_TEST_DIR}:/var/lib/postgresql/data ${POSTGRES_IMAGE}
+. brator_test.env.sh
+docker run --detach -e POSTGRES_USER=${BRATOR_DB_USER} -e POSTGRES_PASSWORD=${BRATOR_DB_PASSWORD} -p 5432:5432 -v ${BRATOR_TEST_DIR}:/var/lib/postgresql/data ${POSTGRES_IMAGE}
